1bb47db338338751e6af61695a13ab168becd742
anil.angirekula <anil.angirekula@RMKDT038.CHSPLDC.COM>
2017-04-26 18:29:37 +0530

    initial commit for this project

---
+++ DemoProj/src/TestProj.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author anil.angirekula
+ *
+ */
+public class TestProj {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("This is sample project");
+	}
+
+}
