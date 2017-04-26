3a4a5a4b7f454c2d56b78703795913abe6a33c83
anil.angirekula <anil.angirekula@RMKDT038.CHSPLDC.COM>
2017-04-26 18:41:06 +0530

    commit for second time

--- DemoProj/src/TestProj.java
+++ DemoProj/src/TestProj.java
@@ -13,7 +13,7 @@ public class TestProj {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("This is sample project andfirst project");
+  System.out.println("This is sample project and first project");
 	}
 
 }
