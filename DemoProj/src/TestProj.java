b41b6d447e064249d40cb63a72d144897ca5a2c6
aniluser <anil.angirekula@callhealth.co.in>
2017-04-26 18:37:49 +0530

    performed edit option for first time

--- DemoProj/src/TestProj.java
+++ DemoProj/src/TestProj.java
@@ -13,7 +13,7 @@ public class TestProj {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-  System.out.println("This is sample project");
+  System.out.println("This is sample project andfirst project");
 	}
 
 }
