484b07609d6516a30545ef081b1c328406674d2e
aniluser <anil.angirekula@callhealth.co.in>
2017-04-27 12:24:46 +0530

    Edit one more time

--- DemoProj/src/TestProj.java
+++ DemoProj/src/TestProj.java
@@ -14,6 +14,7 @@ public class TestProj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("This is sample project and first project");
+		System.out.println("We are deleting the project");
 	}
 
 }
